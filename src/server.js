fc490e7b8664c645f55cba4e469b84dac5a929cc
Gabriel Acevedo <gacevedo@gmail.com>
2024-10-03 16:50:18 -0500

    Fixing src/server.js for finding 21

--- src/server.js
+++ src/server.js
@@ -9,14 +9,11 @@ const registerViewRoutes = require('./views');
 
 const app = express();
 const port = process.env.PORT || 8088;
-const SESSION_SECRET_KEY = 'kjhdkd-sjkhsjsh-kjshshkdhsk-jsjhd';
 
 const tarpitEnv = {
-  sessionSecretKey: process.env.SESSION_SECRET_KEY || SESSION_SECRET_KEY,
+  sessionSecretKey: process.env.SESSION_SECRET_KEY,
   applicationPort: process.env.PORT || 8088
 };
-
-app.set('tarpitEnv', tarpitEnv);
 
 // Insider attack
 const insider = function(req, res, next) {
@@ -32,28 +29,14 @@ const insider = function(req, res, next) {
   next();
 };
 
-app.use(insider);
-
-app.use(function(err, req, res, next) {
-  logger.error(err.stack);
-  res.status(500).send('Something broke!');
-});
-
-// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
-
-// parse application/json
 app.use(bodyParser.json());
-
 app.use(cookieParser());
-
-app.use(
-  session({
-    secret: SESSION_SECRET_KEY,
-    resave: false,
-    saveUninitialized: false
-  })
-);
+app.use(session({
+  secret: process.env.SESSION_SECRET_KEY,
+  resave: false,
+  saveUninitialized: false
+}));
 
 app.set('view engine', 'pug');
 app.set('views', `./src/Views`);
@@ -66,3 +49,5 @@ app.listen(port, () =>
     `Tarpit App listening on port ${port}!. Open url: http://localhost:${port}`
   )
 );
+
+
